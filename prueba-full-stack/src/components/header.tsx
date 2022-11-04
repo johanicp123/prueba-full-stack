87a6e5e1fe643fbc8b0317f841e24a28afab70b1
johanicp123 <johanicp123@gmail.com>
2022-11-04 17:45:57 -0500

    cambio header

--- prueba-full-stack/src/components/header.tsx
+++ prueba-full-stack/src/components/header.tsx
@@ -3,7 +3,6 @@ import Logo from "../assets/img/12.jpg";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 
 const Header = () => {
   return (
